b79673def42bd0f4e758239a4db6b64dfeb65232
vvbaby1122 <59265996+vvbaby1122@users.noreply.github.com>
2020-06-13 18:57:09 -0400

    Update InverseMatrix.R

--- InverseMatrix.R
+++ InverseMatrix.R
@@ -1,3 +1,7 @@
+##This first function makes a empty matrix x and set m to NULL, the set and get functions withinin this makeCacheMatrix function
+##set a matrix, stores it in another environment and calls the function on x. 
+##Then, it set the inverse of the matrix and stores it in another environment.
+##Finally, it lists out the results accordingly.
 makeCacheMatrix <- function(x=matrix()) {
     m <- NULL
     set <- function(y) {
@@ -10,6 +14,8 @@ makeCacheMatrix <- function(x=matrix()) {
     list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
+##This second function index matrix x by another function getinverse, then filters out the null matrices. Then solve the inverse of
+##a given matrix.
 cacheSolve <- function(x,...) {
     m <- x$getinverse()
     if (!is.null(m)) {
